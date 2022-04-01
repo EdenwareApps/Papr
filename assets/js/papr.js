f0f5d8a73368b0ba262d78b8273cc35ee5f83b1f
Efox <efox.web@gmail.com>
2022-04-01 13:01:20 -0300

    v1.0.4
    - Fixed base domain
    - Default background image changed

--- assets/js/papr.js
+++ assets/js/papr.js
@@ -584,7 +584,7 @@ jQuery(window).on('close', () => {
 jQuery(document).ready(() => {
 	jQuery.ajax({
 		type: "GET",
-		url: 'http://papr.ml/pad.xml',
+		url: 'http://megacubo.tv/papr/pad.xml',
 		dataType: "xml",
 		success: (xml) => {
 			let v = xml.getElementsByTagName('Program_Version')
@@ -598,7 +598,7 @@ jQuery(document).ready(() => {
 					if(data && data.version){
 						installedVersion = data.version
 						if(installedVersion < currentVersion){
-							let url = 'http://papr.ml/?version='+installedVersion
+							let url = 'http://megacubo.tv/papr/?version='+installedVersion
 							if(confirm(Lang.NEW_VERSION_AVAILABLE)){
 								nw.Shell.openExternal(url)
 							} else {
